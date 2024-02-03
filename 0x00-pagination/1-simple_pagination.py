5758ff098d7c4faf39006ee06387632e0261bf47
TosineBadges <tosinebadges@gmail.com>
2024-02-02 17:46:09 -0800

    completed work

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,46 +1,46 @@
 #!/usr/bin/env python3
-“””Simple pagination sample.
-“””
-Import csv
-From typing import List, Tuple
+"""Simple pagination sample.
+"""
+import csv
+from typing import List, Tuple
 
 
-Def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    “””Retrieves the index range from a given page and page size.
-    “””
-    Start = (page – 1) * page_size
-    End = start + page_size
-    Return (start, end)
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Retrieves the index range from a given page and page size.
+    """
+    start = (page - 1) * page_size
+    end = start + page_size
+    return (start, end)
 
 
-Class Server:
-    “””Server class to paginate a database of popular baby names.
-    “””
-    DATA_FILE = “Popular_Baby_Names.csv”
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
 
-    Def __init__(self):
-        “””Initializes a new Server instance.
-        “””
-        Self.__dataset = None
+    def __init__(self):
+        """Initializes a new Server instance.
+        """
+        self.__dataset = None
 
-    Def dataset(self) -> List[List]:
-        “””Cached dataset
-        “””
-        If self.__dataset is None:
-            With open(self.DATA_FILE) as f:
-                Reader = csv.reader(f)
-                Dataset = [row for row in reader]
-            Self.__dataset = dataset[1:]
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
 
-        Return self.__dataset
+        return self.__dataset
 
-    Def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        “””Retrieves a page of data.
-        “””
-        Assert type(page) == int and type(page_size) == int
-        Assert page > 0 and page_size > 0
-        Start, end = index_range(page, page_size)
-        Data = self.dataset()
-        If start > len(data):
-            Return []
-        Return data[start:end]
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Retrieves a page of data.
+        """
+        assert type(page) == int and type(page_size) == int
+        assert page > 0 and page_size > 0
+        start, end = index_range(page, page_size)
+        data = self.dataset()
+        if start > len(data):
+            return []
+        return data[start:end]
